26023f5b39332c295742a8184e5284e5b39047ed
ScottiBR <guilhermescotti@gmail.com>
2019-07-15 22:34:40 -0300

    Updates

---
+++ precache-manifest.c80131eb9d0d88421b895aa36759ded4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1424cd27e105ecf318d1ca6346919f24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "f86f5acca8f84eecb9c1",
+    "url": "/static/js/main.6726f0a2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ static/js/main.6726f0a2.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{3:function(e,n,t){e.exports=t(9)},9:function(e,n,t){"use strict";t.r(n);var r=t(0),a=t.n(r),l=t(2),o=t.n(l),c=a.a.createContext(),u=function(){return a.a.createElement(c.Provider,null,a.a.createElement("div",null,a.a.createElement("h1",null,"Test")))};o.a.render(a.a.createElement(u,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.6726f0a2.chunk.js.map
